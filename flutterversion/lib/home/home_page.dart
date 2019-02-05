ff9d56738e9b2202c1354be7131d284023cec817
Mike KAI <1043932993@qq.com>
2019-02-05 17:02:11 +0800

    部分重构

--- flutterversion/lib/home/home_page.dart
+++ flutterversion/lib/home/home_page.dart
@@ -2,11 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:dio/dio.dart';
 import 'package:flutterversion/global_config.dart';
 import 'dart:core';
-import 'dart:io';
 import '../bean/DataHub.dart';
 import '../bean/Item.dart';
 import '../home/banner_view.dart';
 import '../home/item_view.dart';
+import '../net/network_manager.dart';
+import '../home/title_bar.dart';
 /*
  * @Created Date: 2019-01-26 17:06
  * @Author: Ckai
@@ -26,6 +27,8 @@ class _HomePageState extends State<HomePage> {
   List<Item> bannerList = <Item>[];
   Dio dio = new Dio();
   bool isRequsetedItem = false;
+  HttpManager dioManager = HttpManager.instance;
+  bool isVisible = false;
 
   ScrollController _controller = new ScrollController();
 
@@ -44,6 +47,8 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
+    final double statusBarHeight = MediaQuery.of(context).padding.top;
+    final double bottomHeight = MediaQuery.of(context).padding.bottom;
     if (dataHub != null && !isRequsetedItem) {
       loadMore();
       isRequsetedItem = true;
@@ -57,79 +62,59 @@ class _HomePageState extends State<HomePage> {
               )
             : new RefreshIndicator(
                 onRefresh: _onRefresh,
-                child: new SingleChildScrollView(
-                  controller: _controller,
-                  physics: new ClampingScrollPhysics(),
-                  child: new Column(
-                    children: <Widget>[
-                      bannerView(context, bannerList),
-                      itemView(context, itemList),
-                    ],
-                  ),
+                child: new Stack(
+                  children: <Widget>[
+                    new SingleChildScrollView(
+                      controller: _controller,
+                      physics: new ClampingScrollPhysics(),
+                      child: new Column(
+                        children: <Widget>[
+                          bannerView(context, bannerList),
+                          itemView(context, itemList),
+                        ],
+                      ),
+                    ),
+                    //new TitlebarWidget(isVisible, "asdaswqeqweqweas"),
+                  ],
                 ),
               ),
       ),
     );
   }
 
-  Future<Null> _onRefresh() async {
+  Future<void> _onRefresh() async {
     bannerList.clear();
     itemList.clear();
     getBanner();
     isRequsetedItem = false;
   }
 
-  void getBanner() async {
-    try {
-      dio.options.headers["User-Agent"] =
-          "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.71 Safari/537.36";
-      dio.options.connectTimeout = 8000;
-      Response response;
-      response = await dio.get(GlobalConfig.BASEAPI +
-          GlobalConfig.BANNER +
-          "&date=" +
-          currentTimeMillis().toString());
-      if (response.statusCode == HttpStatus.ok) {
-        var data = response.data;
-        dataHub = DataHub.fromJson(data);
-        bannerList.addAll(dataHub.issueList[0].itemList);
+  Future<void> getBanner() async {
+    dataHub = await dioManager.doGetData(GlobalConfig.BASEAPI +
+        GlobalConfig.BANNER +
+        "&date=" +
+        currentTimeMillis().toString());
 
-        var toRemove = [];
-        bannerList.forEach((item) {
-          if (item.type == "banner2") {
-            toRemove.add(item);
-          }
-        });
-        //删除toRemove中的元素
-        bannerList.removeWhere((e) => toRemove.contains(e));
-        setState(() {});
+    bannerList.addAll(dataHub.issueList[0].itemList);
+
+    var toRemove = [];
+    bannerList.forEach((item) {
+      if (item.type == "banner2") {
+        toRemove.add(item);
       }
-    } catch (e) {
-      return print(e);
-    }
+    });
+    //删除toRemove中的元素
+    bannerList.removeWhere((e) => toRemove.contains(e));
+    setState(() {});
   }
 
-  void loadMore() async {
-    try {
-      dio.options.headers["User-Agent"] =
-          "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.71 Safari/537.36";
-      dio.options.connectTimeout = 8000;
-      Response response;
-      if (dataHub != null) {
-        response = await dio.get(dataHub.nextPageUrl);
-        if (response.statusCode == HttpStatus.ok) {
-          var data = response.data;
-          dataHub = DataHub.fromJson(data);
+  Future<void> loadMore() async {
+    dataHub = await dioManager.doGetData(dataHub.nextPageUrl);
+    itemList.addAll(dataHub.issueList[0].itemList);
 
-          itemList.addAll(dataHub.issueList[0].itemList);
-          print(dataHub.nextPageUrl);
-          setState(() {});
-          //print(dataHub.issueList[0].itemList[0].data.text);
-        }
-      }
-    } catch (e) {
-      return print(e);
-    }
+    print(dataHub.nextPageUrl);
+    setState(() {});
+    //print(dataHub.issueList[0].itemList[0].data.text);
   }
 
   static int currentTimeMillis() {
---
+++ flutterversion/lib/net/network_manager.dart
@@ -0,0 +1,46 @@
+import 'package:dio/dio.dart';
+import 'dart:io';
+import '../bean/DataHub.dart';
+import '../bean/Item.dart';
+/*
+ * @Created Date: 2019-02-05 14:24
+ * @Author: Ckai
+ * @Description: 
+ */
+
+class HttpManager {
+  factory HttpManager() => _getInstance();
+  static HttpManager get instance => _getInstance();
+  static HttpManager _instance;
+  Dio dio;
+  Response response;
+  DataHub dataHub;
+  List<Item> list = <Item>[];
+
+  HttpManager._internal() {
+    dio = new Dio();
+    dio.options.headers["User-Agent"] =
+        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.71 Safari/537.36";
+    dio.options.connectTimeout = 8000;
+  }
+
+  static HttpManager _getInstance() {
+    if (_instance == null) {
+      _instance = new HttpManager._internal();
+    }
+    return _instance;
+  }
+
+  Future<DataHub> doGetData(String url) async {
+    try {
+      response = await dio.get(url);
+      if (response.statusCode == HttpStatus.ok) {
+        var data = response.data;
+        dataHub = DataHub.fromJson(data);
+      }
+    } catch (e) {
+      print(e);
+    }
+    return dataHub;
+  }
+}
